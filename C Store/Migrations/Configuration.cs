d9b3df6ffed3277c301f76e92e81388eada9bd40
gwentzel <ggwentzel@hotmail.com>
2017-03-25 14:54:22 -0400

    Added navigation between products and orders

--- C Store/Migrations/Configuration.cs
+++ C Store/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace C_Store.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using C_Store;
 
     internal sealed class Configuration : DbMigrationsConfiguration<C_Store.StoreContext>
     {
@@ -12,19 +13,19 @@ namespace C_Store.Migrations
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(C_Store.StoreContext context)
+        protected override void Seed(C_Store.StoreContext StoreContext)
         {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
+            
+            //    StoreContext.Products.AddOrUpdate(
+            //      p => p.Name,
+                //  new product { Name = "apple" },
+                //  new Product { Name = "orange" }
+         
+            //   );
             //
         }
     }
